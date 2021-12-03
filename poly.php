347e0c118985c125018282e91e439992d2ddec4c
RidwanAdiP24 <93441895+RidwanAdiP24@users.noreply.github.com>
2021-12-03 10:35:25 +0700

    Add files via upload

---
+++ poly.php
@@ -0,0 +1,51 @@
+<?php
+abstract class bola{
+    public $jariJari;
+    public $pi = 3.14;
+  
+    public function __construct($jariJari){
+        $this->jariJari = $jariJari;
+    }
+
+   abstract public function hitung_bola();
+}
+
+interface detail_bola{
+	 public function nilai_bola();
+}
+  
+class Voli extends bola implements detail_bola{
+   public function hitung_bola(){
+	   $x = 4*$this->jariJari * $this->jariJari * $this->pi;
+     return "Luas Permukaan Bola Voli adalah $x";
+   }
+   
+   public function nilai_bola(){
+	   return "<br> Menghitung Luas Permukaan Bola Voli <br>";
+	   }
+}
+
+class basket extends bola implements detail_bola{
+   public function hitung_bola(){
+		$x = 4*$this->jariJari * $this->jariJari * $this->pi;
+     return "Luas Permukaan Bola Basket adalah $x";
+   }
+   
+   public function nilai_bola(){
+	   return "<br> Menghitung Luas Permukaan Bola Basket <br>";
+	   }
+}
+
+function cetak_detail_bola($objek_detail){
+	 return $objek_detail->nilai_bola();
+}
+
+$bola_Voli = new Voli(14);
+echo $bola_Voli->hitung_bola();
+echo cetak_detail_bola($bola_Voli);
+echo "<hr>";
+
+$bola_basket = new basket(18);
+echo $bola_basket->hitung_bola();
+echo cetak_detail_bola($bola_basket);
+?>
